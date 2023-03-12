cb840e0a99798e8ca70a77392b7a8ad37e89cb2c
Aleksey-Dr <aprel181818@gmail.com>
2023-03-12 19:22:38 +0200

    Task 2. Points 1, 2
    Task 2. Points 1, 2
    1. Creating and render the markup on base the array values "galleryItems".
    2. Сonnecting the script and library styles.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Point 1
+// crutches
+const gallery = document.querySelector('.gallery');
+gallery.remove();
+const paragraph = document.querySelector('p');
+const galleryNew = document.createElement('div');
+paragraph.after(galleryNew);
+galleryNew.classList.add('gallery');
+
+createGalleryItems();
+
+function createGalleryItems() {
+    const items = [];
+
+    for (let i = 0; i < galleryItems.length; i++) {
+
+        const item = document.createElement('a');
+        item.href = galleryItems[i].original;
+        item.classList.add('gallery__item');
+
+        const image = document.createElement('img');
+        item.append(image);
+        image.src = galleryItems[i].preview;
+        image.alt = galleryItems[i].description;
+        image.classList.add('gallery__image');
+
+        items.push(item);
+    }
+
+    galleryNew.append(...items);
+}
