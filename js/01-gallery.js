51914c4d96d3a761fd9eeb2f101b1ab7fc83c18b
Aleksey-Dr <aprel181818@gmail.com>
2023-03-12 10:55:54 +0200

    Task 1. Point 2
    Point 2.Event delegation to div.gallery and get url a big image.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,6 +2,8 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Point 1. Creating and render the markup on base the array values "galleryItems"
 const gallery = document.querySelector('.gallery');
 
 // console.log(gallery);
@@ -16,6 +18,7 @@ function createGalleryItems() {
 
         const link = document.createElement('a');
         link.href = galleryItems[i].original;
+        // link.rel = "noreferrer noopener";
         link.classList.add('gallery__link');
         item.append(link);
 
@@ -23,11 +26,23 @@ function createGalleryItems() {
         link.append(image);
         image.src = galleryItems[i].preview;
         image.alt = galleryItems[i].description;
+        image.setAttribute('data-source', galleryItems[i].original);
+        // console.log(image.dataset.source);
         image.classList.add('gallery__image');
 
         items.push(item);
     }
 
     gallery.append(...items);
-    console.log(items);
+    // console.log(items);
+}
+
+// Point 2.Event delegation to div.gallery and get url a big image
+
+gallery.addEventListener('click', getBigImage);
+
+function getBigImage(event) {
+    const selectedImage = event.target.dataset.source;
+    // console.log(selectedImage);
+    // console.log(event.currentTarget);
 }
